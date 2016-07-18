70506d6f96091f34cc7de5e7d6c64417c802281d
IRONHACKJOAQUIN <ironhackjoaquin@gmail.com>
2016-07-18 20:51:42 +0200

    inicial

---
+++ test.rb
@@ -0,0 +1,15 @@
+
+#ESTE ES MI PRIMER PROGRAMA - ESTOS SON COMENTARIOS. 
+##Componentes  keywords, metodos, cadenas, variables
+#-
+
+nombre_curso = "Curso ddRuby"
+#variables siempre en minuscula , no se declaran solo inizializar 
+
+
+nombre_curso = "Curso de Ajax"
+
+puts "hola ruby"
+puts "soy la segunda linea"
+puts "ADIOS ADIOSjjjj"
+
